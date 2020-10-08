d4e11d8fba3d62e52598cfa645cb26e51a06aa18
titivince <vincent25200009@gmail.com>
2020-10-08 09:34:32 +0200

    first push

---
+++ integration.js
@@ -0,0 +1,17 @@
+var block1 = document.getElementById("col1");
+var block2 = document.getElementById("col2");
+var block3 = document.getElementById("col3");
+var end = document.getElementById("foot");
+
+function textBlock1(){
+    col1.scrollIntoView(true);
+}
+function textBlock2(){
+    col2.scrollIntoView(true);
+}
+function textBlock3(){
+    col3.scrollIntoView(true);
+}
+function textBlock4(){
+    foot.scrollIntoView(true);
+}
